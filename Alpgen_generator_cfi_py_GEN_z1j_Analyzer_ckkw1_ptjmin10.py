73df7c6ee07941c90be4825184df1658662f0d2a
Yun-Ju <yunju@cern.ch>
2014-07-21 11:08:44 +0200

    settings for alpgen and MadGraph

---
+++ Alpgen_generator_cfi_py_GEN_z1j_Analyzer_ckkw1_ptjmin10.py
@@ -0,0 +1,134 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.18 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/Generator/python/Alpgen_generator_cfi.py -s GEN --conditions=START53_V7G::All --eventcontent=RECOSIM --mc -n 100 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+# Input source
+#process.source = cms.Source("EmptySource")
+process.source = cms.Source("AlpgenSource",
+        fileNames = cms.untracked.vstring('file:../Z1jets/z1j_ptjmin10')
+)
+
+
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.18 $'),
+    annotation = cms.untracked.string('Configuration/Generator/python/Alpgen_generator_cfi.py nevts:100'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('/afs/cern.ch/work/y/yunju/private/MySamples/AlpgenValid201407/GenSamples/Z1j_Alpgen_Pythia6_generator_cfi_py_GEN_ickkw1_ptjmin10.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7G::All', '')
+
+process.generator = cms.EDFilter("Pythia6HadronizerFilter",
+    jetMatching = cms.untracked.PSet(
+        exclusive = cms.bool(True),
+        scheme = cms.string('Alpgen'),
+        etMin = cms.double(25.0),
+        applyMatching = cms.bool(True),
+        drMin = cms.double(0.7)
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    comEnergy = cms.double(8000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model', 
+            'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'PARP(82)=1.8387   ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1960. ! sqrts for which PARP82 is set', 
+            'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter', 
+            'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter', 
+            'PARP(90)=0.16  ! Multiple interactions: rescaling power', 
+            'PARP(67)=2.5    ! amount of initial-state radiation', 
+            'PARP(85)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(86)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(62)=1.25   ! ', 
+            'PARP(64)=0.2    ! ', 
+            'MSTP(91)=1      !', 
+            'PARP(91)=2.1   ! kt distribution', 
+            'PARP(93)=15.0  ! '),
+        processParameters = cms.vstring('MSEL=0          ! User defined processes', 
+            'MSTJ(1)=1       ! Fragmentation/hadronization on or off', 
+            'MSTJ(11)=3      ! Choice of the fragmentation function'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+process.demo = cms.EDAnalyzer('YJMutiGenJetAna'
+,SetWZ_PY=cms.int32(23),
+SetHadronizer_PY=cms.int32(1)
+
+)
+process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string('histo_Z1j_Alpgen_Pythia6_generator_cfi_py_GEN_ickkw1_ptjmin10_match1.root')
+  )
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RECOSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq * process.demo
