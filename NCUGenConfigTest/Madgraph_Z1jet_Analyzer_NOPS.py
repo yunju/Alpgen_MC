5b4d9401ac2198cb536fc8a97b6ea9ca151ed4b6
Yun-Ju <yunju@cern.ch>
2014-07-23 14:23:55 +0200

    Read madgraph LHE and run gen level without partonshower

---
+++ NCUGenConfigTest/Madgraph_Z1jet_Analyzer_NOPS.py
@@ -0,0 +1,147 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.18 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/Generator/python/Alpgen_generator_cfi.py -s GEN --conditions=START53_V7G::All --eventcontent=RECOSIM --mc -n 100 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+#process.source = cms.Source("EmptySource")
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring("file:../../LHE_MadGraph/Z1jet/events.lhe")
+)
+
+
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.18 $'),
+    annotation = cms.untracked.string('Configuration/Generator/python/Alpgen_generator_cfi.py nevts:100'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('/afs/cern.ch/work/y/yunju/private/MySamples/AlpgenValid201407/GenSamples/Z1jetMadgraph_p6had_generator_cfi_py_GEN_Analyzer_matching_off_fortest.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7G::All', '')
+
+process.generator = cms.EDFilter("Pythia6HadronizerFilter",
+#    jetMatching = cms.untracked.PSet(
+#       scheme = cms.string("Madgraph"),
+#       mode = cms.string("auto"),       # soup, or "inclusive" / "exclusive"
+#       MEMAIN_etaclmax = cms.double(5.0),
+#       MEMAIN_qcut = cms.double(10),
+#       MEMAIN_minjets = cms.int32(0),
+#       MEMAIN_maxjets = cms.int32(1),
+#       MEMAIN_showerkt = cms.double(0),  # use 1=yes only for pt-ordered showers !
+#       MEMAIN_nqmatch = cms.int32(5),    #PID of the flavor until which the QCD radiation are kept in the matching procedure; 
+                                                                # if nqmatch=4, then all showered partons from b's are NOT taken into account
+                                                                # Note(SdV): The default must be 5, it defines which maximal PDG code is considered as taking part in the jet matching procedure.
+#       MEMAIN_excres = cms.string(""),       # list of resonances to exclude, for example "1000021,1000001", etc.
+#       outTree_flag = cms.int32(0)               # 1=yes, write out the tree for future sanity check
+       
+
+
+ #   ),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    comEnergy = cms.double(8000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model', 
+            'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'PARP(82)=1.8387   ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1960. ! sqrts for which PARP82 is set', 
+            'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter', 
+            'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter', 
+            'PARP(90)=0.16  ! Multiple interactions: rescaling power', 
+            'PARP(67)=2.5    ! amount of initial-state radiation', 
+            'PARP(85)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(86)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(62)=1.25   ! ', 
+            'PARP(64)=0.2    ! ', 
+            'MSTP(91)=1      !', 
+            'PARP(91)=2.1   ! kt distribution', 
+            'PARP(93)=15.0  ! '),
+        processParameters = cms.vstring('MSEL=0          ! User defined processes', 
+            'MSTJ(1)=0       ! Fragmentation/hadronization on or off', 
+            'MSTJ(11)=3      ! Choice of the fragmentation function',
+            'MSTP(61)=0      ! Parton showering on or off'
+            ),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+process.demo = cms.EDAnalyzer('YJMutiGenJetAna'
+,SetWZ_PY=cms.int32(23),
+SetHadronizer_PY=cms.int32(1)
+
+)
+process.TFileService = cms.Service("TFileService",
+                                       fileName = cms.string('histo_MadGraph_pythia6z1j_Pythia6_ps0_had0_ma0.root')
+  )
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+#process.demo_step=cms.EndPath(process.demo)
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RECOSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq * process.demo 
+
